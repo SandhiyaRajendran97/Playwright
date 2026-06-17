811d299b75554b449dde20bf0700f351634f248b
sandhiyarajendran97 <sandhiyarajendran.97@outlook.com>
2026-06-17 22:20:21 +0530

    Alert

--- tests/alert.spec.js
+++ tests/alert.spec.js
@@ -1,6 +1,5 @@
 import {test,expect} from '@playwright/test'
-
-test("simple alert",async({page})=>{
+ test("simple alert",async({page})=>{
     await page.goto("https://testautomationpractice.blogspot.com/")
 
     page.on('dialog',async dialog=>{
@@ -14,3 +13,51 @@ test("simple alert",async({page})=>{
     await page.locator('//button[@id="alertBtn"]').click()
     await page.pause()
 })
+
+
+test("confirmation",async({page})=>{
+    await page.goto("https://testautomationpractice.blogspot.com/")
+
+    page.on('dialog',async confirm=>{
+        console.log(confirm.type())
+        console.log(confirm.message());
+        expect(confirm.message("Press a button!"))
+
+        await confirm.accept()
+    })
+
+    await page.locator('//button[@id="confirmBtn"]').click()
+    await page.pause()
+})
+
+    test("confirmation alert",async({page})=>{
+    await page.goto("https://testautomationpractice.blogspot.com/")
+
+    page.on('dialog',async confirm=>{
+        console.log(confirm.type())
+        console.log(confirm.message());
+        expect(confirm.message("Press a button!"))
+
+        await confirm.dismiss()
+    })
+     await page.locator('//button[@id="confirmBtn"]').click()
+    await expect(page.locator('//p[@id="demo"]')).toHaveText("You pressed Cancel!")
+    await page.pause()
+})
+ 
+   test("prompt alert",async({page})=>{
+    await page.goto("https://testautomationpractice.blogspot.com/")
+
+    page.on('dialog',async prompt=>{
+        console.log(prompt.type())
+        console.log(prompt.message());
+        expect(prompt.message("Press a button!"))
+
+        await prompt.accept("Playwright")
+    })
+     await page.locator('//button[@id="promptBtn"]').click()
+    await expect(page.locator('//p[@id="demo"]')).toHaveText("Hello Playwright! How are you today?")
+    await page.pause()
+})
+
+

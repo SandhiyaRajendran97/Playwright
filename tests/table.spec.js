f3bb4b8a92c02e6d3d36a947afca830e1564d074
sandhiyarajendran97 <sandhiyarajendran.97@outlook.com>
2026-06-23 22:30:36 +0530

    table

---
+++ tests/table.spec.js
@@ -0,0 +1,26 @@
+import {test,expect} from '@playwright/test'
+
+test ("web table",async({page})=>{
+    await page.goto("https://testautomationpractice.blogspot.com/")
+
+    const column= page.locator('//table[@id="productTable"]/thead/tr/th')
+    const row = page.locator('//table[@id="productTable"]/tbody/tr')
+
+    console.log("column count :",await column.count()); //4
+    console.log("Row count :",await row.count()); //5
+
+
+//single
+//await page.locator('//table[@id="productTable"]/tbody/tr/td[text()="Tablet "]/following-sibling::td/child::input[@type="checkbox"]')
+
+const locat = await row.filter({
+    has:page.locator("td"),
+    hasText:"Tablet"
+
+
+})
+
+await locat.locator('//input[@type="checkbox"]').check()
+await page.pause()
+
+})

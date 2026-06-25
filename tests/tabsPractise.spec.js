1c8ee6518741403878c90cccd95c02af841a9385
sandhiyarajendran97 <sandhiyarajendran.97@outlook.com>
2026-06-25 22:50:36 +0530

    Screenshot

---
+++ tests/tabsPractise.spec.js
@@ -0,0 +1,34 @@
+import{test,expect, chromium} from '@playwright/test'
+    test("Amazon",async()=>{
+        const browser= await chromium.launch()
+        const context=await browser.newContext()
+        const page=await context.newPage()
+        const newPage=await context.newPage()
+
+    await page.goto("https://www.amazon.in/")
+    console.log(await page.title())
+
+    await page.waitForTimeout(3000)
+
+    await newPage.goto("https://www.amazon.in/SHREIT")
+    console.log(await newPage.title())
+
+     await page.waitForTimeout(3000) 
+
+})
+
+test.only("page navigation",async({page})=>{
+    await page.goto("https://www.flipkart.com/")
+    await page.waitForTimeout(2000)
+
+    await page.goto("https://www.flipkart.com/mobile-phones")
+    await page.waitForTimeout(2000)
+
+    await page.goBack()
+    await page.waitForTimeout(2000)
+
+    await page.goForward()
+    await page.waitForTimeout(2000)
+
+
+})

a2cfb661113902f4332da8fe4cd3d155163bbb25
sandhiyarajendran97 <sandhiyarajendran.97@outlook.com>
2026-06-29 22:57:31 +0530

    pom

---
+++ tests/pom.spec.js
@@ -0,0 +1,17 @@
+import {test,expect} from '@playwright/test'
+import Orange from './pages/hrm'
+
+test("Login using orangeHrm",async({page})=>{
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+
+    let loginHrm =new Orange(page);
+
+    loginHrm.login("Admin","admin123");
+
+    await page.pause()
+
+
+
+
+
+})

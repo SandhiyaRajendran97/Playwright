344cd5a45817f9512c997800081ae6def07435e8
sandhiyarajendran97 <sandhiyarajendran.97@outlook.com>
2026-07-14 22:07:03 +0530

    Frames

--- tests/Frames.spec.js
+++ tests/Frames.spec.js
@@ -8,3 +8,14 @@ test('Frame', async ({ page }) => {
 
     await frame.locator("input[name='fname']").fill('Sandhiya');
 });
+
+test.only('Enter Name', async ({ page }) => {
+
+    await page.goto('https://letcode.in/frame');
+
+    const frame = page.frameLocator('#firstFr');
+
+    await frame.locator('input[name="fname"]').fill('Sandhiya');
+    await frame.locator('input[name="lname"]').fill('Rajendran');
+
+});

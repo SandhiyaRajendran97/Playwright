333ebaad8aa404442508d11345fadf97357829d9
sandhiyarajendran97 <sandhiyarajendran.97@outlook.com>
2026-07-19 21:34:33 +0530

    assertion

--- tests/assertion.spec.js
+++ tests/assertion.spec.js
@@ -17,3 +17,8 @@ test("assertion",async({page})=>{
     await expect(page.locator(inputLocator)).toHaveValue("playwright")
     //await expect(page.locator(inputLocator)).toBeEmpty()
 })
+
+test.only('Verify Title', async ({ page }) => {
+    await page.goto('https://playwright.dev/');
+    await expect(page).toHaveTitle(/Playwright/);
+});
